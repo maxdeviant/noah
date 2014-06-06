228c0714bb14f73a386b202714c33e52ad47fddc
Marshall Bowers <elliott.codes@gmail.com>
2014-06-06 19:34:01 -0400

    Get verb forms for verbs.

--- noah/generate.py
+++ noah/generate.py
@@ -19,17 +19,53 @@ with open('../raws/2of12id.txt') as wordlist:
         # Get part of speech
         pos = split.pop(0).replace(':', '')
 
-        if (pos == 'N' and len(split) > 0):
-            plurals = split.pop(0)
+        if pos == 'N':
+            plurals = ''
 
-        dictionary.append({
-            'word': word,
-            'plurals': plurals,
-            'part_of_speech': pos,
-            'dump': split[0:len(split)]
-        })
+            if len(split) > 0:
+                plurals = split.pop(0)
 
-        plurals = ''
+            dictionary.append({
+                'word': word,
+                'plurals': plurals,
+                'part_of_speech': pos,
+                'dump': split[0:len(split)]
+            })
+        elif pos == 'V':
+            verbs = []
+            if len(split) > 2:
+                for v in split[:]:
+                    i = split.index(v)
+                    if not '(' in split[i]:
+                        verbs.append(split.pop(i))
+
+                dictionary.append({
+                    'word': word,
+                    'verb_forms': {
+                        'past': verbs[0] ,
+                        'present': verbs[1],
+                        'plural': verbs[2]
+                    },
+                    'part_of_speech': pos,
+                    'dump': split[0:len(split)]
+                })
+            else:
+                dictionary.append({
+                    'word': word,
+                    'verb_forms': {
+                        'past': '' ,
+                        'present': '',
+                        'plural': ''
+                    },
+                    'part_of_speech': pos,
+                    'dump': split[0:len(split)]
+                })
+        else:
+            dictionary.append({
+                'word': word,
+                'part_of_speech': pos,
+                'dump': split[0:len(split)]
+            })
 
 with open('../dictionaries/english.json', 'w') as output:
     json.dump(dictionary, output, indent=4, separators=(',', ': '))
