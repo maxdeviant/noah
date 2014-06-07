9ee9ba34e447e99c868fcb43d40ce905cebf5fb9
Marshall Bowers <elliott.codes@gmail.com>
2014-06-06 20:12:53 -0400

    Add list and define functions.

--- noah/noah.py
+++ noah/noah.py
@@ -1,4 +1,24 @@
 import json
 
 class Noah(object):
-    pass
+    def __init__(self, dictionary_file):
+        self.dictionary = json.load(dictionary_file)
+
+    def list(self):
+        return '\n'.join([entry['word'] for entry in self.dictionary])
+
+    def define(self, word):
+        entry = next((x for x in self.dictionary if x['word'] == word), None)
+
+        if not entry is None:
+            return '%s (%s)' % (entry['word'], entry['part_of_speech'])
+
+def main():
+    with open('../dictionaries/english.json') as dictionary:
+        n = Noah(dictionary)
+
+    print n.list()
+    print n.define('aardvark')
+
+if __name__ == '__main__':
+    main()
